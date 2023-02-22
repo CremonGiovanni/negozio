c7b90ac052cb8e6c441f6db6ae5b007cf216fd62
cremon.giovanni <cremon.giovanni@studenti.salesianisesto.it>
2023-02-22 08:49:31 +0100

    create addProdotto

---
+++ addProdotto.php
@@ -0,0 +1,21 @@
+<?php
+include("lib/connectDB.php");
+include("conf/confDB.php");
+$conn=connectDB($hostname, $username, $password, $DBname);
+if(!$conn){
+    echo"immpossibile connettersi";
+}else{
+    $nome=$_POST["nome"];
+    $des=$_POST["des"];
+    $costo=$_POST["costo"];
+    $sql= "insert into scuola values('$nome', '$des', $costo)";
+    //echo $sql;
+    $query_run=mysqli_query($conn,$sql);
+    if($query_run){
+        echo "inserimento riuscito";
+        exit(0);
+    }else{
+        echo "impossibile aggiungere la scuola $sql";
+    }
+};
+?>
