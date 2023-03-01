30fc7daac9eb35a66833ac12f72b986ae456ec20
cremon.giovanni <cremon.giovanni@studenti.salesianisesto.it>
2023-03-01 09:11:31 +0100

    create other select Prodotto

---
+++ selectProdotto5.php
@@ -0,0 +1,24 @@
+<?php
+include("lib/connectDB.php");
+include("conf/confDB.php");
+$conn=connectDB($hostname, $username, $password, $DBname);
+if(!$conn){
+    echo "impossibile connettersi";
+}else{
+    $sql="SELECT * FROM Prodotto WHERE nome='penna'costo>50 AND costo<120";
+    $result = mysqli_query($conn,$sql);
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<thead><tr><th>ID</th><th>Nome</th><th>Descrizione</th><th>Prezzo</th></tr></thead>";
+        echo "<tbody>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["nome"] . "</td><td>" . $row["descrizione"] . "</td><td>" . $row["costo"] . "</td></tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+    mysqli_close($conn);
+};
+?>
